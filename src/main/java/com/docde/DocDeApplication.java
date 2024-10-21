bb0d2f500772ca6a979e02c9fa234fbf5a499602
KJG04 <freedom7113@gmail.com>
2024-10-21 17:16:37 +0900

    initial commit

---
+++ src/main/java/com/docde/DocDeApplication.java
@@ -0,0 +1,13 @@
+package com.docde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocDeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docde/DocDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
