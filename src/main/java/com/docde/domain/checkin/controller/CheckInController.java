f73fe1daa3e525fb3487f0be2df1b8f4aa08d81b
20240729 <yoohyeonsik@gmail.com>
2024-10-22 14:36:17 +0900

    ADD : 접수하기 제작중

--- src/main/java/com/docde/domain/checkin/controller/CheckInController.java
+++ src/main/java/com/docde/domain/checkin/controller/CheckInController.java
@@ -1,12 +1,13 @@
 package com.docde.domain.checkin.controller;
 
+import com.docde.domain.auth.entity.UserDetailsImpl;
+import com.docde.domain.checkin.dto.CheckInRequest;
 import com.docde.domain.checkin.dto.CheckInResponse;
 import com.docde.domain.checkin.service.CheckInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +18,13 @@ public class CheckInController {
 
     // 접수하기
     @PostMapping("/{hospitalId}/checkin")
-    public ResponseEntity<CheckInResponse> saveCheckIn(){}
+    public ResponseEntity<CheckInResponse> saveCheckIn(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long hospitalId,
+            @RequestBody CheckInRequest checkInRequest
+            ){
+        return ResponseEntity.ok(checkInService.saveCheckIn(userDetails, hospitalId, checkInRequest));
+    }
     // 자신의 접수 상태 확인(사용자)
     // 접수 상태 확인(병원)
     // 접수 상태 변경
--- src/main/java/com/docde/domain/checkin/dto/CheckInRequest.java
+++ src/main/java/com/docde/domain/checkin/dto/CheckInRequest.java
@@ -4,5 +4,5 @@ import lombok.Getter;
 
 @Getter
 public class CheckInRequest {
-    private String status;
+    private Long doctorId;
 }
