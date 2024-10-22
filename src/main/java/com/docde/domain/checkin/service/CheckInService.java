9270f9d929a9884ff9062e3e421e890bb8c1b540
20240729 <yoohyeonsik@gmail.com>
2024-10-22 14:36:17 +0900

    ADD : 접수하기 제작중

--- src/main/java/com/docde/domain/checkin/service/CheckInService.java
+++ src/main/java/com/docde/domain/checkin/service/CheckInService.java
@@ -1,5 +1,7 @@
 package com.docde.domain.checkin.service;
 
+import com.docde.domain.auth.entity.UserDetailsImpl;
+import com.docde.domain.checkin.dto.CheckInRequest;
 import com.docde.domain.checkin.dto.CheckInResponse;
 import com.docde.domain.checkin.entity.CheckIn;
 import com.docde.domain.checkin.repository.CheckInRepository;
@@ -14,6 +16,20 @@ public class CheckInService {
     private final CheckInRepository checkInRepository;
 
     // 접수하기
+    public CheckInResponse saveCheckIn(
+            UserDetailsImpl userDetails,
+            Long hospitalId,
+            CheckInRequest checkInRequest
+    ) {
+
+        // 레포지토리에서 병원 찾기
+
+        // 레포지토리에서 의사 찾기(의사가 null이 아닐 경우)
+        if(checkInRequest.getDoctorId() != null){}
+
+
+    }
+
     // 자신의 접수 상태 확인(사용자)
     // 접수 상태 확인(병원)
     // 접수 상태 변경
@@ -28,5 +44,7 @@ public class CheckInService {
                 checkIn.getCreatedAt()
         );
     }
+
+
 
 }
