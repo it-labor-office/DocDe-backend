a91eb5b97021d7542d0f5f220837d3fe50f2f1b1
KJG04 <freedom7113@gmail.com>
2024-10-23 10:29:06 +0900

    FEAT: 회원가입 예외 처리

--- src/main/java/com/docde/common/exceptions/InvalidRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.docde.common.exceptions;
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
