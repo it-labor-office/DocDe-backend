3310296df3afd1e2b2850a08f3bb0df91bf89223
KJG04 <freedom7113@gmail.com>
2024-10-23 18:48:50 +0900

    FEAT: 이메일 인증 기능

--- src/main/java/com/docde/domain/auth/dto/AuthRequest.java
+++ src/main/java/com/docde/domain/auth/dto/AuthRequest.java
@@ -1,27 +1,28 @@
 package com.docde.domain.auth.dto;
 
 import com.docde.common.enums.Gender;
-import com.docde.domain.auth.dto.AuthRequest.DoctorSignUp;
-import com.docde.domain.auth.dto.AuthRequest.PatientSignUp;
-import com.docde.domain.auth.dto.AuthRequest.ReissueToken;
-import com.docde.domain.auth.dto.AuthRequest.SignIn;
+import com.docde.domain.auth.dto.AuthRequest.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public sealed interface AuthRequest permits PatientSignUp, DoctorSignUp, SignIn, ReissueToken {
+public sealed interface AuthRequest permits PatientSignUp, DoctorSignUp, SignIn, ReissueToken, AuthenticateEmail {
     record PatientSignUp(@NotBlank @Email String email, @NotBlank String password, @NotBlank String name,
                          @NotBlank String address, @NotBlank String phone,
-                         @NotNull Gender gender) implements AuthRequest {
+                         @NotNull Gender gender, @NotBlank String code) implements AuthRequest {
     }
 
     record DoctorSignUp(@NotBlank @Email String email, @NotBlank String password, @NotBlank String name,
-                        @NotBlank String description, @NotNull Boolean isDoctorPresident) implements AuthRequest {
+                        @NotBlank String description, @NotNull Boolean isDoctorPresident,
+                        @NotBlank String code) implements AuthRequest {
     }
 
     record SignIn(@NotBlank @Email String email, @NotBlank String password) implements AuthRequest {
     }
 
     record ReissueToken(@NotBlank String refreshToken) implements AuthRequest {
+    }
+
+    record AuthenticateEmail(@NotBlank @Email String email) implements AuthRequest {
     }
 }
