cee3008b3cc1ae429f09c7b13b1df137d57d7cdf
BoSung308 <qhtjd2719@naver.com>
2024-10-23 13:52:50 +0900

    진료기록 엔티티와 레퍼지토리, 진료기록-의사/진료기록-환자 연관관계 설정

--- src/main/java/com/docde/domain/patient/entity/Patient.java
+++ src/main/java/com/docde/domain/patient/entity/Patient.java
@@ -1,10 +1,13 @@
 package com.docde.domain.patient.entity;
 
 import com.docde.common.enums.Gender;
+import com.docde.domain.medicalRecord.entity.MedicalRecord;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -26,6 +29,9 @@ public class Patient {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     Gender gender;
+
+    @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
+    private List<MedicalRecord> medicalRecord;
 
     @Builder
     public Patient(String name, String address, String phone, Gender gender) {
