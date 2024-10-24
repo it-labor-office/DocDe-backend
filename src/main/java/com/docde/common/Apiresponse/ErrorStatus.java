f8479251b364b61e022880fb462cf7e6f169de65
BoSung308 <qhtjd2719@naver.com>
2024-10-24 17:46:07 +0900

    UPDATE:진료기록 CRUD 구현(포스트맨O,테스트코드X)

--- src/main/java/com/docde/common/Apiresponse/ErrorStatus.java
+++ src/main/java/com/docde/common/Apiresponse/ErrorStatus.java
@@ -51,10 +51,16 @@ public enum ErrorStatus implements BaseCode {
     _ALREADY_DONE_RESERVATION(HttpStatus.BAD_REQUEST, 400, "이미 진료가 끝난 예약입니다."),
     _DENIED_RESERVATION(HttpStatus.BAD_REQUEST, 400, "진료가 거부된 예약입니다."),
     _ALREADY_RESERVED_RESERVATION(HttpStatus.BAD_REQUEST, 400, "이미 예약이 승인 되었습니다."),
-    //의사 관련 코드
+
+    // 의사 관련 코드
     _NOT_FOUND_DOCTOR(HttpStatus.NOT_FOUND, 404, "의사를 찾을 수 없습니다."),
-    //환자 관련 코드
+    _FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, 403, "해당 병원에 소속된 의사가 아닙니다."),
+
+    // 환자 관련 코드
     _NOT_FOUND_PATIENT(HttpStatus.NOT_FOUND, 404, "환자를 찾을 수 없습니다."),
+
+    // 진료 관련 코드
+    _NOT_FOUND_MEDICAL_RECORD(HttpStatus.NOT_FOUND, 404, "진료기록을 찾을 수 없습니다."),
 
     //접수 관련 코드
     _BAD_REQUEST_ALREADY_CHECKED_IN(HttpStatus.BAD_REQUEST, 400, "이미 진행중인 접수가 있습니다."),
--- src/main/java/com/docde/domain/medicalRecord/repository/MedicalRecordRepository.java
+++ src/main/java/com/docde/domain/medicalRecord/repository/MedicalRecordRepository.java
@@ -1,14 +1,10 @@
 package com.docde.domain.medicalRecord.repository;
 
-//import com.docde.domain.medicalRecord.dto.response.PatientMedicalRecordResponseDto;
-
 import com.docde.domain.medicalRecord.entity.MedicalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
 
     /*@Query("SELECT new com.docde.domain.medicalRecord.dto.response.PatientMedicalRecordResponseDto" +
@@ -16,7 +12,11 @@ public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Lo
             "FROM MedicalRecord m JOIN m.doctor d WHERE m.patient.id = :patientId")
     List<PatientMedicalRecordResponseDto> findByPatientId(Long patientId);*/
 
+
+
     List<MedicalRecord> findByDoctorId(Long doctorId);
 
     List<MedicalRecord> findByPatientId(Long patientId);
+
+
 }
