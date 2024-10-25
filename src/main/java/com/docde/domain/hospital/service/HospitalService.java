bcba15fb5bfebc1680852e3cfb529386b517ad1c
KJG04 <freedom7113@gmail.com>
2024-10-25 13:59:22 +0900

    TEST: AuthController 테스트 코드 작성

--- src/main/java/com/docde/domain/hospital/service/HospitalService.java
+++ src/main/java/com/docde/domain/hospital/service/HospitalService.java
@@ -1,7 +1,6 @@
 package com.docde.domain.hospital.service;
 
 import com.docde.common.Apiresponse.ErrorStatus;
-import com.docde.common.dto.response.ResponseDto;
 import com.docde.common.exceptions.ApiException;
 import com.docde.domain.hospital.dto.request.HospitalPostRequestDto;
 import com.docde.domain.hospital.dto.request.HospitalWeeklyTimetablePostRequestDto;
@@ -18,19 +17,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class HospitalService {
     private final HospitalRepository hospitalRepository;
     private final WeekTimetableRepository weekTimetableRepository;
+
     @Transactional
     public HospitalPostResponseDto postHospital(HospitalPostRequestDto requestDto, UserDetails userDetails) {
         //유저 권한 확인
         boolean check = checkDoctorPresident(userDetails);
         //병원 갯수는 누구를 기준으로 할까??
-        if(!check) {
+        if (!check) {
             throw new ApiException(ErrorStatus._FORBIDDEN);
         }
         //받은 정보를 바탕으로 병원데이터생성
@@ -43,17 +42,18 @@ public class HospitalService {
         return userDetails.getAuthorities().stream().anyMatch(authority ->
                 authority.getAuthority().equals("ROLE_DOCTOR_PRESIDENT"));
     }
+
     @Transactional
     public HospitalWeeklyTimetablePostResponseDto postWeeklyTimetable(
             HospitalWeeklyTimetablePostRequestDto requestDto,
             UserDetails userDetails, Long hospitalId) {
         boolean check = checkDoctorPresident(userDetails);
-        if(!check) {
+        if (!check) {
             throw new ApiException(ErrorStatus._FORBIDDEN);
         }
 
         Hospital hospital = hospitalRepository.findById(hospitalId).orElseThrow(
-                ()->new ApiException(ErrorStatus._NOT_FOUND_HOSPITAL)
+                () -> new ApiException(ErrorStatus._NOT_FOUND_HOSPITAL)
         );
 
         WeekTimetable weekTimetable = new WeekTimetable(hospital);
@@ -70,7 +70,7 @@ public class HospitalService {
         **/
         //이걸 매핑해서 리스트로 변환
         List<HospitalTimetable> timetables = requestDto.getTimetables().stream().
-                map(dto->{
+                map(dto -> {
                     return new HospitalTimetable(
                             dto.getDayOfTheWeek(),
                             dto.getOpenTime(),
