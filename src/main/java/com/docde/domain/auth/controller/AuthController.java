a6cb7e554d9b5c3abcdb8473dfe9284b406f1790
KJG04 <freedom7113@gmail.com>
2024-10-27 21:16:05 +0900

    TEST: AuthController 테스트 코드 추가

--- src/main/java/com/docde/domain/auth/controller/AuthController.java
+++ src/main/java/com/docde/domain/auth/controller/AuthController.java
@@ -9,8 +9,10 @@ import com.docde.domain.patient.dto.PatientResponse;
 import com.docde.domain.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,23 +21,27 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/signup/patient")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<AuthResponse.PatientSignUp> patientSignUp(@RequestBody @Valid AuthRequest.PatientSignUp signUpRequestDto) {
         User user = authService.patientSignUp(signUpRequestDto.email(), signUpRequestDto.password(), signUpRequestDto.name(), signUpRequestDto.address(), signUpRequestDto.phone(), signUpRequestDto.gender(), signUpRequestDto.code());
         return ApiResponse.onCreated(new AuthResponse.PatientSignUp(user.getId(), user.getEmail(), user.getUserRole(), new PatientResponse(user.getPatient())));
     }
 
     @PostMapping("/auth/signup/doctor")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<AuthResponse.DoctorSignUp> doctorSignUp(@RequestBody @Valid AuthRequest.DoctorSignUp signUpRequestDto) {
         User user = authService.doctorSignUp(signUpRequestDto.email(), signUpRequestDto.password(), signUpRequestDto.name(), signUpRequestDto.description(), signUpRequestDto.isDoctorPresident(), signUpRequestDto.code());
         return ApiResponse.onCreated(new AuthResponse.DoctorSignUp(user.getId(), user.getEmail(), user.getUserRole(), new DoctorResponse(user.getDoctor())));
     }
 
     @PostMapping("/auth/refresh")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<AuthResponse.SignIn> reissueToken(@RequestBody @Valid AuthRequest.ReissueToken reissueTokenRequestDto) {
         return ApiResponse.onCreated(authService.reissueToken(reissueTokenRequestDto.refreshToken()));
     }
 
     @PostMapping("/auth/email-authentication")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse authenticateEmail(@RequestBody @Valid AuthRequest.AuthenticateEmail authenticateEmailRequestDto) {
         authService.authenticateEmail(authenticateEmailRequestDto.email());
         return ApiResponse.onCreated(null);
