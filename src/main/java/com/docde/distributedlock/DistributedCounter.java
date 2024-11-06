7cab1b87fb5d54e61f624a004d7a4f83560fb80c
BoSung308 <qhtjd2719@naver.com>
2024-11-06 19:14:49 +0900

    del:counter클래스 및 현식님과 aop 합치면서 삭제한 클래스들

--- src/main/java/com/docde/distributedlock/DistributedCounter.java
+++ src/main/java/com/docde/distributedlock/DistributedCounter.java
@@ -1,16 +1,38 @@
+/*
 package com.docde.distributedlock;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 @Entity
 public class DistributedCounter {
 
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private int count;
-}
+
+    @Column(nullable = false, unique = true)
+    private Long doctorId;
+
+    @Column(nullable = false)
+    private LocalDateTime reservationTime; // 예약 시간
+
+    public DistributedCounter(Long doctorId, int count, LocalDateTime reservationTime) {
+        this.doctorId = doctorId;
+        this.count = count;
+        this.reservationTime = reservationTime;
+    }
+
+    public DistributedCounter increment() {
+        this.count++; // 카운트 증가 및 자신 참조 반환
+        return this;
+    }
+}*/
