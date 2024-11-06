3018aa5e3ed67c864488b301b5f416a858e1b0f7
BoSung308 <qhtjd2719@naver.com>
2024-11-06 19:29:46 +0900

    update:접수요청 aop와 예약요청 aop를 한 클래스에 병합

--- src/main/java/com/docde/config/RedisConfig.java
+++ src/main/java/com/docde/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.docde.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,5 +19,14 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        // Redis 서버 주소 설정
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+        return Redisson.create(config);
     }
 }
