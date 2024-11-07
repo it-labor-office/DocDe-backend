f6e54b3b90cc135d749dbdf541e5e385a283cf78
20240729 <yoohyeonsik@gmail.com>
2024-11-07 16:27:10 +0900

    TEST : 접수 컨트롤러

--- src/test/java/com/docde/domain/checkin/controller/CheckInControllerTest.java
+++ src/test/java/com/docde/domain/checkin/controller/CheckInControllerTest.java
@@ -5,13 +5,11 @@ import com.docde.config.JwtAuthenticationToken;
 import com.docde.config.JwtUtil;
 import com.docde.config.SecurityConfig;
 import com.docde.domain.auth.entity.AuthUser;
-import com.docde.domain.auth.service.AuthService;
 import com.docde.domain.checkin.dto.CheckInRequest;
 import com.docde.domain.checkin.dto.CheckInResponse;
+import com.docde.domain.checkin.dto.CheckInResponseOfPatient;
 import com.docde.domain.checkin.service.CheckInService;
-import com.docde.domain.hospital.controller.HospitalController;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +19,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CheckInController.class)
@@ -45,37 +44,51 @@ class CheckInControllerTest {
     @MockBean
     private CheckInService checkInService;
 
-    @BeforeEach
-    void setUp() {
-    }
+    AuthUser patientAuthUser = new AuthUser(
+            1L,
+            "e@ma.il",
+            UserRole.ROLE_PATIENT,
+            null,
+            1L,
+            null
+    );
+    JwtAuthenticationToken patientToken = new JwtAuthenticationToken(patientAuthUser);
+
+    AuthUser doctorAuthUser = new AuthUser(
+            2L,
+            "e2@ma.il",
+            UserRole.ROLE_DOCTOR_PRESIDENT,
+            1L,
+            null,
+            1L
+    );
+    JwtAuthenticationToken doctorToken = new JwtAuthenticationToken(doctorAuthUser);
+
+    CheckInRequest checkInRequest = new CheckInRequest();
+
+    CheckInResponse checkInResponse = new CheckInResponse(
+            1L,
+            "patientName",
+            null,
+            LocalDateTime.now(),
+            "WAITING"
+    );
+
+    CheckInResponseOfPatient checkInResponseOfPatient = new CheckInResponseOfPatient(
+            "patientName",
+            "doctorName",
+            LocalDateTime.now(),
+            1L
+    );
 
     @Test
     void saveCheckIn() throws Exception {
-        AuthUser patientAuthUser = new AuthUser(
-                1L,
-                "e@ma.il",
-                UserRole.ROLE_PATIENT,
-                null,
-                1L,
-                null
-        );
-        JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(patientAuthUser);
-
-        CheckInRequest checkInRequest = new CheckInRequest();
-
-        CheckInResponse checkInResponse = new CheckInResponse(
-                1L,
-                "patientName",
-                null,
-                LocalDateTime.now(),
-                "WAITING"
-        );
 
         Mockito.when(checkInService.saveCheckIn(patientAuthUser, 1L, checkInRequest))
                 .thenReturn(checkInResponse);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/hospitals/1/checkin")
-                .with(SecurityMockMvcRequestPostProcessors.authentication(jwtAuthenticationToken))
+        mockMvc.perform(post("/hospitals/1/checkin")
+                .with(SecurityMockMvcRequestPostProcessors.authentication(patientToken))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jacksonObjectMapper.writeValueAsString(checkInRequest))
                 .with(csrf())
@@ -83,26 +96,71 @@ class CheckInControllerTest {
     }
 
     @Test
-    void getMyCheckIn() {
+    void getMyCheckIn() throws Exception {
+
+        Mockito.when(checkInService.getMyCheckIn(patientAuthUser, 1L)).thenReturn(checkInResponseOfPatient);
+
+        mockMvc.perform(get("/hospitals/1/checkin")
+                .with(authentication(patientToken))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jacksonObjectMapper.writeValueAsString(checkInRequest))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 
     @Test
-    void getQueue() {
+    void getQueue() throws Exception {
+
+        List<Object> queue = new ArrayList<>();
+        Mockito.when(checkInService.getQueue(doctorAuthUser, 1L)).thenReturn(queue);
+
+        mockMvc.perform(get("/hospitals/1/checkin/simple")
+                .with(authentication(patientToken))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 
     @Test
-    void getAllCheckIns() {
+    void getAllCheckIns() throws Exception {
+
+        List<CheckInResponse> checkInResponseList = new ArrayList<>();
+        Mockito.when(checkInService.getAllCheckIns(doctorAuthUser, 1L)).thenReturn(checkInResponseList);
+
+        mockMvc.perform(get("/hospitals/1/checkin/all")
+                .with(authentication(patientToken))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 
     @Test
-    void updateCheckIn() {
+    void updateCheckIn() throws Exception {
+
+        Mockito.when(checkInService.updateCheckIn(doctorAuthUser, 1L, 1L, checkInRequest))
+                .thenReturn(checkInResponse);
+
+        mockMvc.perform(put("/hospitals/1/checkin/1")
+                .with(authentication(patientToken))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jacksonObjectMapper.writeValueAsString(checkInRequest))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 
     @Test
-    void resetNumber() {
+    void resetNumber() throws Exception {
+
+        mockMvc.perform(put("/hospitals/1/checkin/reset")
+                .with(authentication(doctorToken))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 
     @Test
-    void deleteCheckIn() {
+    void deleteCheckIn() throws Exception {
+
+        mockMvc.perform(delete("/hospitals/checkin/1")
+                .with(authentication(doctorToken))
+                .with(csrf())
+        ).andExpect(status().isOk());
     }
 }
