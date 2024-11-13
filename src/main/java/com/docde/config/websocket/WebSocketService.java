bb90518ec40fa6dc27ac7d8f5f04d12191cb7014
20240729 <yoohyeonsik@gmail.com>
2024-11-13 12:19:05 +0900

    add : 일단 긁어 옴+/ws 경로 permit

---
+++ src/main/java/com/docde/config/websocket/WebSocketService.java
@@ -0,0 +1,36 @@
+package com.docde.config.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
+
+@Service
+@RequiredArgsConstructor
+public class WebSocketService {
+
+    private final WebSocketHandler webSocketHandler;
+    private final WebSocketListener webSocketListener;
+
+    public void addSession(WebSocketSession session) {
+        webSocketHandler.addSession(session);
+    }
+
+    public void removeSession(WebSocketSession session) {
+        webSocketHandler.removeSession(session);
+    }
+
+    public void handlePayload(WebSocketSession session, String payload) {
+        if (payload.startsWith("subscribe:")) {
+            String topic = payload.substring("subscribe:".length()).trim();
+            subscribeToTopic(session, topic);
+            return;
+        }
+
+        webSocketHandler.sendMessageToTopic(session, payload);
+    }
+
+    private void subscribeToTopic(WebSocketSession session, String topic) {
+        webSocketHandler.registerSessionTopic(session, topic);
+        webSocketListener.subscribeToTopic(topic);
+    }
+}
